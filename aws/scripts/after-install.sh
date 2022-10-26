1f2d8c916c23937fef83d4ecc93e65f07d1de6aa
Przemek <przemek.grabarek@venturelabs.team>
2022-10-26 19:21:25 +0200

    changed war to jar

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://DeploymentBucket/VideoStreamApplication.war /usr/local/tomcat9/webapps/VideoStreamApplication.war
+aws s3 cp s3://DeploymentBucket/VideoStreamApplication.jar /usr/local/tomcat9/webapps/VideoStreamApplication.jar
 
 
 # Ensure the ownership permissions are correct.
